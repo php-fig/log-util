2bda4992857186f1801d16f520ae15cd5367c06f
Anton Ukhanev <xedin.unknown@gmail.com>
2021-06-12 14:51:08 +0200

    Include the tests
    This begins porting as per
    https://github.com/php-fig/log/pull/76#issuecomment-858743302

---
+++ tests/LoggerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Psr\Log\Test;
+
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LogLevel;
+
+class LoggerTest extends TestCase
+{
+    /**
+     * @return \PHPUnit\Framework\MockObject\MockObject|TestLogger
+     */
+    protected function createSubject()
+    {
+        $mock = $this->getMockBuilder('Psr\Log\Test\TestLogger')
+            ->enableProxyingToOriginalMethods()
+            ->getMock();
+
+        return $mock;
+    }
+
+    public function testLog()
+    {
+        $message = uniqid('message');
+        $level = LogLevel::INFO;
+        $subject = $this->createSubject();
+
+        $subject->log($level, $message, array());
+
+        $this->assertTrue($subject->hasRecord($message, $level));
+    }
+}
