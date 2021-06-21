c06e6856a3cb2462d49acd562334307210b7c521
Anton Ukhanev <xedin.unknown@gmail.com>
2021-06-21 21:52:46 +0200

    Allow class re-declaration
    In this particular scenario, this is necessary to make the creation of tests easier.

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -7,6 +7,7 @@ if (class_exists('PHPUnit\Framework\TestCase')) {
     {
     }
 } elseif (class_exists('PHPUnit_Framework_TestCase')) {
+    // phpcs:ignore PSR1.Classes.ClassDeclaration.MultipleClasses
     class AbstractTestCase extends \PHPUnit_Framework_TestCase
     {
     }
